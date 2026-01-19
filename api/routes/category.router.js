4112b3de0161a043465714a7b5dbd9067e04e6af
Lucas Metz <lucas.metz00@gmail.com>
2026-01-19 12:53:35 +0100

    Adding validateId middleware and unit test

--- api/routes/category.router.js
+++ api/routes/category.router.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import categoryController from '../controllers/category.controller.js';
+import { validateId } from '../middlewares/common.middleware.js';
 
 const router = Router();
 
 router.get("/categories", categoryController.getAll);
-router.get("/categories/:id", categoryController.getById);
-router.delete("/categories/:id", categoryController.delete);
+router.get("/categories/:id", validateId, categoryController.getById);
+router.delete("/categories/:id", validateId, categoryController.delete);
 router.post("/categories", categoryController.create);
-router.patch("/categories/:id", categoryController.update);
+router.patch("/categories/:id", validateId, categoryController.update);
 
 
 export default router;
