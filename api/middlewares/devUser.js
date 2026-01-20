8dbd78997bb94810975932a3a193a4d0dbfc086c
Lucas Metz <lucas.metz00@gmail.com>
2026-01-20 14:26:08 +0100

    deleting useless files

--- api/middlewares/devUser.js
+++
@@ -1,7 +0,0 @@
-export default function devUser(req, res, next) {
-  // TEMP: user id fixe (ou via header)
-  // const userId = Number(req.headers["x-user-id"]) || 1;
-
-  req.user = { id: 1 };
-  next();
-}
