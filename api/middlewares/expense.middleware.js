a5d5ea88c692eba9e7d1ffd417b104b068fffaed
nassim <nassim@MacBook-Pro-de-nassim.local>
2026-01-21 11:18:04 +0100

    expense.middleware ok possibilité d' amelioration _message d'erreur personalisée_

---
+++ api/middlewares/expense.middleware.js
@@ -0,0 +1,40 @@
+import Joi from 'joi';
+
+export function validateCreateExpense (req, res, next){
+
+    const createExpenseSchema = Joi.object ({
+         title : Joi.string().required(),
+         amount : Joi.number().positive().required(),
+         date : Joi.date().iso().required(),
+         category_id : Joi.number().min(1).optional()// categorie pas obligatoire au moment de la creation une depense peut etre nue de categories
+
+
+    });
+
+        const validation = createExpenseSchema.validate(req.body)
+
+        if(validation.error){
+        return res.status(400).json({ error: validation.error });
+        }
+
+    next();
+}
+
+export function validateUpdateExpense (req, res, next){
+
+    const updateExpenseSchema = Joi.object ({
+         title : Joi.string().optional(),
+         amount : Joi.number().positive().optional(),
+         date : Joi.date().iso().optional(), // date au format YYYY-MM-DD
+         category_id : Joi.number().min(1).optional()
+
+    }).min(1) // au moins un champs requis a voir si on maintient
+
+        const validation = updateExpenseSchema.validate(req.body)
+
+        if(validation.error){
+        return res.status(400).json({ error: validation.error });
+        }
+
+    next();
+}
